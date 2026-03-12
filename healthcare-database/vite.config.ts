8829f1e33ea7147ed6db092e847e4ecd34dc688b
Tanay S Chithore <tanaychithore@mailbox.unideb.hu>
2026-03-12 11:14:21 +0100

    Necessary changes to source pointers

--- healthcare-database/vite.config.ts
+++ healthcare-database/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import {fileURLToPath, URL} from 'url'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@types': path.resolve(__dirname, './src/types'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@': fileURLToPath(new URL('./src',import.meta.url)),
+      '@pages':fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@types':fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
     },
   },
   server: {
